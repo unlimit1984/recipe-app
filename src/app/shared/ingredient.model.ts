db63de8d0466c402990b175b0b9566d7a42e383b
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2019-12-08 23:12:45 +0100

    Prepare 57 lesson

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,15 @@
+export class Ingredient {
+  // public name: string;
+  // public amount: number;
+  //
+  // constructor(name: string, amount: number) {
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
+
+  // is the same as
+
+  constructor(public name: string, public amount: number) {
+  }
+
+}
