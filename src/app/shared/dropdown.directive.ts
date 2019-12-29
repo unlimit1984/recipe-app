44c82d96be80f825c5137e70b2f5a6679c24ae51
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2019-12-29 01:38:38 +0100

    Implement dropdown directive in lesson 102

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,10 +1,37 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
+// export class DropdownDirective implements OnInit {
+// My solution
+// @Input() isOpened: boolean = false;
+//
+// constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+// }
+//
+// ngOnInit(): void {
+//   this.handleOpening();
+// }
+//
+// @HostListener('click') mouseClick(eventData: Event) {
+//   this.isOpened = !this.isOpened;
+//   this.handleOpening();
+// }
+//
+// handleOpening() {
+//   if (this.isOpened) {
+//     this.renderer.addClass(this.elementRef.nativeElement, 'open');
+//   } else {
+//     this.renderer.removeClass(this.elementRef.nativeElement, 'open');
+//   }
+// }
+
+// Author's solution
 export class DropdownDirective {
+  @HostBinding('class.open') isOpened = false;
 
-  constructor() { }
-
+  @HostListener('click') toggleOpen() {
+    this.isOpened = !this.isOpened;
+  }
 }
