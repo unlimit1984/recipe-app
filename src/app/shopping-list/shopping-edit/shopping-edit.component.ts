39f4ec46ac08330244ba69853cf845c867c35bca
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-02-22 16:28:18 +0100

    [TD Forms] 223 Implement clear button handler

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -41,7 +41,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   //   const newIngredient = new Ingredient(ingName, ingAmount);
   //   this.shoppingListService.addIngredient(newIngredient);
   // }
-  onAddItem(f: NgForm) {
+  onSubmit(f: NgForm) {
     const value = f.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
@@ -49,6 +49,13 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.shoppingListService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    this.slForm.reset();
+  }
+
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
   }
 
   ngOnDestroy(): void {
