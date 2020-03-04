85899ad9aa84158b9c4d1a307065774ef08ae04f
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-03-04 22:36:06 +0100

    [ReactiveForms] 237 Implement removing ingredient button

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -104,4 +104,8 @@ export class RecipeEditComponent implements OnInit {
   onCancel() {
     this.router.navigate(['../'], {relativeTo: this.route});
   }
+
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
 }
