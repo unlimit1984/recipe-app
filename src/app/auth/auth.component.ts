603e3ffd4e011641d5e7b1189ba809ced0db9610
unlimit1984 <unlimit_rmc@mail.ru>
2020-07-27 23:49:51 +0200

    [authentication] 293 Sending the sign up request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -8,12 +9,31 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent {
   isLoginMode: boolean = true;
 
+  constructor(private authService: AuthService) {
+  }
+
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(authForm: NgForm) {
-    console.log(authForm.value);
+    if (!authForm.valid) {
+      return;
+    }
+    const email = authForm.value.email;
+    const password = authForm.value.password;
+
+    if (this.isLoginMode) {
+      // ...
+    } else {
+      this.authService.signup(email, password)
+        .subscribe(resData => {
+          console.log(resData);
+        }, error => {
+          console.log(error);
+        });
+      // console.log(authForm.value);
+    }
     authForm.reset();
   }
 }
