8afc5e81727224dbaf5c811bb25a45ede94c0a0a
unlimit1984 <unlimit_rmc@mail.ru>
2020-08-04 23:42:57 +0200

    [authentication] 299 Reflect Auth state on UI

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,34 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   // @Output() featureSelected = new EventEmitter<string>();
   //
   // onSelect(feature: string) {
   //   this.featureSelected.emit(feature);
   // }
 
-  constructor(private dataStorageService: DataStorageService) {
+  isAuthenticated = false;
+  private userSub: Subscription;
+
+  ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user => {
+      // this.isAuthenticated = !user ? false : true;
+      this.isAuthenticated = !!user;  //the same
+      console.log('!user', !user);
+      console.log('!!user', !!user);
+    });
   }
+
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {
+  }
+
   onSaveData() {
     this.dataStorageService.storeRecipes();
   }
@@ -21,4 +36,9 @@ export class HeaderComponent {
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
   }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
+
 }
