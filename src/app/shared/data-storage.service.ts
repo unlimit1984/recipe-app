e31b98c1b0eaf331534e729ebfe7855b06697c40
unlimit1984 <unlimit_rmc@mail.ru>
2020-08-05 10:44:33 +0200

    [authentication] 300 Add the token to Outgoing request

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { map, tap } from 'rxjs/operators';
+import { exhaustMap, map, take, tap } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {
+  constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {
   }
 
   storeRecipes() {
@@ -19,15 +20,33 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.http.get<Recipe[]>('https://ng-course-recipe-book-7fe57.firebaseio.com/recipes.json')
-      .pipe(map(recipes => {
-          return recipes.map(recipe => {
-            return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
-          })
-        }),
-        tap(recipes => {
-            this.recipeService.setRecipes(recipes);
-          }
-        ));
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap(user => {
+        // return this.http.get<Recipe[]>('https://ng-course-recipe-book-7fe57.firebaseio.com/recipes.json?auth=' + user.token);
+        return this.http.get<Recipe[]>('https://ng-course-recipe-book-7fe57.firebaseio.com/recipes.json',
+          {
+            params: new HttpParams().set('auth', user.token)
+          });
+      }),
+      map(recipes => {
+        return recipes.map(recipe => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+        });
+      }),
+      tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        }
+      ));
+    // return this.http.get<Recipe[]>('https://ng-course-recipe-book-7fe57.firebaseio.com/recipes.json')
+    //   .pipe(map(recipes => {
+    //       return recipes.map(recipe => {
+    //         return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+    //       });
+    //     }),
+    //     tap(recipes => {
+    //         this.recipeService.setRecipes(recipes);
+    //       }
+    //     ));
   }
 }
