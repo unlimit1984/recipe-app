9a82155709b259286565d25e35ef311dab4ca94d
unlimit1984 <unlimit_rmc@mail.ru>
2020-09-28 13:29:32 +0200

    [deploying] 340 use environment variables

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { catchError, tap } from 'rxjs/operators';
 import { BehaviorSubject, throwError } from 'rxjs';
 import { User } from './user.model';
 import { Router } from '@angular/router';
+import { environment} from '../../environments/environment';
 
 export interface AuthResponseData {
   idToken: string;
@@ -25,7 +26,8 @@ export class AuthService {
 
   signup(email: string, password: string) {
     return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBirC5_aMPvVIquVNfmEkqZghzFMXUqsIk',
+      // 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBirC5_aMPvVIquVNfmEkqZghzFMXUqsIk',
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`,
       {email: email, password: password, returnSecureToken: true})
       .pipe(catchError(this.handleError), tap(resData => {
         // const expirationDate = new Date(new Date().getTime() + 1000 * +resData.expiresIn);
@@ -45,7 +47,11 @@ export class AuthService {
 
   login(email: string, password: string) {
     return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBirC5_aMPvVIquVNfmEkqZghzFMXUqsIk',
+      // 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBirC5_aMPvVIquVNfmEkqZghzFMXUqsIk',
+      // Option 1
+      // 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +environment.firebaseAPIKey,
+      // Option 2
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`,
       {email: email, password: password, returnSecureToken: true})
       .pipe(catchError(this.handleError), tap(resData => {
         this.handleAuthentication(
