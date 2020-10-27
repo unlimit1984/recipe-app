b84e188c7deb0e33dc909cce51e3b5448bd0b512
unlimit1984 <unlimit_rmc@mail.ru>
2020-10-27 09:17:25 +0100

    [NgRx] 366 Effects and Error Handling

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,9 +1,42 @@
-import { Actions, ofType } from '@ngrx/effects';
+import { Actions, Effect, ofType } from '@ngrx/effects';
 import * as AuthActions from './auth.actions';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
 
 export class AuthEffects {
-  authLogin = this.actions$.pipe(ofType(AuthActions.LOGIN_START));
+  @Effect()
+  authLogin = this.actions$
+    .pipe(
+      ofType(AuthActions.LOGIN_START),
+      switchMap((authData: AuthActions.LoginStart, index) => {
+        return this.http.post<AuthResponseData>(
+          `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`,
+          {email: authData.payload.email, password: authData.payload.password, returnSecureToken: true})
+          .pipe(
+            catchError(err => {
+              // ...
+              of();
+            }),
+            map(resData => {
+              of();
+            })
+          );
+      })
+    );
 
-  constructor(private actions$: Actions) {
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient) {
   }
 }
