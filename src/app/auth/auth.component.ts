3efca11abb6420efa5ef5c209688e280313d857c
unlimit1984 <unlimit_rmc@mail.ru>
2020-11-09 10:30:56 +0100

    [NgRx] 375 Finish the Auth Effects

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,6 @@
 import { Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
 import { Subscription } from 'rxjs';
-import { Router } from '@angular/router';
 import { AlertComponent } from '../shared/alert/alert.component';
 import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 
@@ -24,8 +22,6 @@ export class AuthComponent implements OnInit, OnDestroy {
   private storeSub: Subscription;
 
   constructor(
-    private authService: AuthService,
-    private router: Router,
     private componentFactoryResolver: ComponentFactoryResolver,
     private store: Store<fromApp.AppState>) {
   }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,25 +3,13 @@ import { Store } from '@ngrx/store';
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from './store/auth.actions';
 
-export interface AuthResponseData {
-  idToken: string;
-  email: string;
-  refreshToken: string;
-  expiresIn: string;
-  localId: string;
-  registered?: boolean;
-}
-
 @Injectable({providedIn: 'root'})
 export class AuthService {
-
-  // user = new BehaviorSubject<User>(null);
   private tokenExpirationTimer: any;
 
   constructor(
     private store: Store<fromApp.AppState>) {
   }
-
 
   setLogoutTimer(expirationDuration: number) {
     console.log('expirationDuration', expirationDuration);
